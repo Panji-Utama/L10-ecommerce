ccf7a4cfc2b73f38d04ed6ac287e4081debe1291
FIRMAN PANJI <fpu2468@gmail.com>
2024-01-02 23:38:42 +0700

    feat: Finished Login Page UI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,28 +4,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gacor | Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8ce2e63a8c.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="login-container">
-        <h2>Welcome to Gacor</h2>
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required autofocus>
+    <div class="login-separator">
+        <div class="left-content">
+            <img src="{{ asset('images/logo.png') }}" alt="" id="logo">
+            <h2>The best and most aesthetic e-commerce <br>website you'll ever use</h2>
+        </div>
+        <div class="right-content">
+            <div class="login-container">
+                <h2>Welcome to Gacor</h2>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <!-- <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" required autofocus>
+                    </div> -->
+                    <div class="input-icon">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" id="email" placeholder="Email" required>
+                    </div>
+                    <div class="input-icon">
+                        <i class="fa-solid fa-lock"></i>
+                        <input type="text" id="email" placeholder="Email" required>
+                    </div>
+                    <div class="login">
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <button type="submit">Login</button>
-            </div>
-        </form>
+        </div>
     </div>
 </body>
 
