54e51a0ccddbd20a8497e66780301c8983e0b211
FIRMAN PANJI <fpu2468@gmail.com>
2024-01-09 23:04:10 +0700

    progress: Navbar #2

--- resources/views/users/home_user.blade.php
+++ resources/views/users/home_user.blade.php
@@ -4,8 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gacor | User Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/home_user.css')}}">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico')}}" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/8ce2e63a8c.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -20,10 +25,14 @@
             <div class="navbar-links">
                 <ul>
                     <li>
-                        <a href="">Nav1</a>
-                        <a href="">Nav2</a>
-                        <a href="">Nav3</a>
+                        <a href="">HOME</a>
+                        <a href="">BEDROOM</a>
+                        <a href="">LIVING ROOM</a>
+                        <a href="">DINING ROOM</a>
+                        <a href="">SPECIALS</a>
                     </li>
+                    <button id="cartBtn"><i class="fa fa-shopping-bag"></i></button>
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
                 </ul>
             </div>
         </div>
